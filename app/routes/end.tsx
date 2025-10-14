3374a68edfabe6ac4e7e9433acc6dcd2c4c10dbf
faithia-anastasia <211831874+faithia-anastasia@users.noreply.github.com>
2025-10-14 18:39:35 +0900

    クリア時のメッセージを追加

--- app/routes/end.tsx
+++ app/routes/end.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from "react-router";
 
-export default function end() {
+/*export default function end() {
   const navigate = useNavigate();
   return (
     <div>
@@ -8,4 +8,32 @@ export default function end() {
       <button onClick={() => navigate("../")}>ホームに戻る</button>
     </div>
   );
+}*/
+
+export default function End(){
+  return(
+    <div className="w-screen h-screen bg-[#091b0c] text-white font-serif">
+
+      <header className="mb-8">
+        <h1 className="p-2 underline decoration-[#FF4500] text-3xl">8番ページ</h1>
+      </header>
+      
+      <div className="text-center">
+        <h2 className="mb-3 text-3xl">
+          <span className="underline decoration-[#FF4500]">8/8</span><br/>
+          クリアおめでとう
+        </h2>
+        <div>
+          8番ページからの脱出おめでとう。<br/>
+          君たちが目にした異変はすべてCSSによって引き起こされたものだ。<br/>
+          <br/>
+          もしも、まだ異変をすべて見つけ切れていないのならば<br/>
+          今一度、異変の正体を確かめてみよう。<br/>
+          <br/>
+          異変をすべて見つけ、異変マスターに、そしてCSSマスターになるのだ。
+        </div>
+      </div>
+
+    </div>
+  )
 }
