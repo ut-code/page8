b6e39614d0677040c65bd05a1217dc595d0d9820
SotaTamura <tamso1868@gmail.com>
2025-10-17 17:41:02 +0900

    ゲーム画面

--- app/routes.ts
+++ app/routes.ts
@@ -2,6 +2,6 @@ import { type RouteConfig, index, route } from "@react-router/dev/routes";
 
 export default [
   index("routes/home.tsx"),
-  route("game/:id", "routes/game.$id.tsx"),
+  route("game", "routes/game.tsx"),
   route("end", "routes/end.tsx"),
 ] satisfies RouteConfig;
