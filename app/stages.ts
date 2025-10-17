a996afc067cb0f7cc5685d6768a4f0e822f7c3dc
SotaTamura <tamso1868@gmail.com>
2025-10-17 18:29:45 +0900

    stages.ts追加

---
+++ app/stages.ts
@@ -0,0 +1,29 @@
+type StageType = {
+  id: number;
+  path: string;
+  keyword: string;
+  detail: string;
+  code: string;
+  image: string;
+  state: "isDetected" | "isNotDetected" | "isNotEncountered";
+};
+export let stages: StageType[] = [
+  {
+    id: 0,
+    path: "/game/a.tsx",
+    keyword: "通常",
+    detail: "通常のページ",
+    code: "",
+    image: "",
+    state: "isDetected",
+  },
+  {
+    id: 1,
+    path: "/game/b.tsx",
+    keyword: "異変",
+    detail: "異変のページ",
+    code: "ihen.code();",
+    image: "/ihen.png",
+    state: "isNotEncountered",
+  },
+];
