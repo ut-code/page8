9c2cfba2c5a5d6f99d805458e3cac961f2de5a36
SotaTamura <tamso1868@gmail.com>
2025-10-19 13:10:47 +0900

    navigateの微調整

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -14,64 +14,92 @@ export function meta({}: Route.MetaArgs) {
 export default function Home() {
   const navigate = useNavigate();
   return (
-    <div style={{
-      backgroundColor:"#091b0c",
-      width:"100vw",
-      height:"100vh"}}>
-      <div style={{
-        display:"flex",
-        justifyContent:"center",
-        color:"#CCCCCC",
-        fontSize:"120px",
-        position:"relative",
-        top:"150px",
-        fontFamily:"serif",
-        textDecoration:"underline",
-        textDecorationColor:"gray"
-      }}>
-        <div style={{
-          display:"grid",
-          gridTemplateColumns:"219px 200px 60px",
-          gridTemplateRows:"80px 80px",
-          alignItems:"center"
-        }}>
-          <div style={{
-            fontSize:"140px",
-            gridColumn:"1/2",
-            gridRow:"1/3"
-          }}>8番</div>
-          <div style={{
-            fontSize:"65px",
-            gridColumn:"2/3",
-            gridRow:"1/2"
-          }}>ページ</div>
-          <div style={{
-            fontSize:"80px",
-            gridColumn:"2/3",
-            gridRow:"2/3",
-            justifySelf:"center"
-          }}>Page</div>
-          <div style={{
-            fontSize:"170px",
-            gridColumn:"3/4",
-            gridRow:"1/3"
-          }}>8</div>
+    <div
+      style={{
+        backgroundColor: "#091b0c",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          color: "#CCCCCC",
+          fontSize: "120px",
+          position: "relative",
+          top: "150px",
+          fontFamily: "serif",
+          textDecoration: "underline",
+          textDecorationColor: "gray",
+        }}
+      >
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "219px 200px 60px",
+            gridTemplateRows: "80px 80px",
+            alignItems: "center",
+          }}
+        >
+          <div
+            style={{
+              fontSize: "140px",
+              gridColumn: "1/2",
+              gridRow: "1/3",
+            }}
+          >
+            8番
+          </div>
+          <div
+            style={{
+              fontSize: "65px",
+              gridColumn: "2/3",
+              gridRow: "1/2",
+            }}
+          >
+            ページ
+          </div>
+          <div
+            style={{
+              fontSize: "80px",
+              gridColumn: "2/3",
+              gridRow: "2/3",
+              justifySelf: "center",
+            }}
+          >
+            Page
+          </div>
+          <div
+            style={{
+              fontSize: "170px",
+              gridColumn: "3/4",
+              gridRow: "1/3",
+            }}
+          >
+            8
+          </div>
         </div>
       </div>
-      <div style={{
-        display:"flex",
-        justifyContent:"center"
-      }}>
-        <button 
+      <div
         style={{
-          position:"relative",
-          fontFamily:"serif",
-          fontSize:"40px",
-          color:"orangered",
-          top:"280px",
-          cursor:"pointer"
+          display: "flex",
+          justifyContent: "center",
         }}
-        onClick={() => navigate("/game")}>ゲームスタート</button>
+      >
+        <button
+          style={{
+            position: "relative",
+            fontFamily: "serif",
+            fontSize: "40px",
+            color: "orangered",
+            top: "280px",
+            cursor: "pointer",
+          }}
+          onClick={() => navigate("/game/0")}
+        >
+          ゲームスタート
+        </button>
       </div>
     </div>
   );
