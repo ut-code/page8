e6bcda5743b471806f0ca19131ab14461ed375f4
faithia-anastasia <211831874+faithia-anastasia@users.noreply.github.com>
2025-10-20 19:39:43 +0900

    コメントアウトを削除

--- app/routes/game.tsx
+++ app/routes/game.tsx
@@ -41,33 +41,9 @@ export default function Game() {
   const location = useLocation();
   const navigate = useNavigate();
   const pageNum = Number(localStorage.getItem("pageNum")); // ページ番号0~8
-  // const [stageInfo, setStageInfo] = useState<StageType>();
-  // const [PageComponent, setPageComponent] = useState<ComponentType<{
-  //   pageNum: number;
-  //   stageId: number;
-  // }> | null>(null);
+
   const stageId = Math.floor(Math.random() * stages.length); // ページの種類のID
-  // useEffect(() => {
-  //   const currentStage = stages.filter((s) => s.id === stageId)[0];
-  //   setStageInfo(currentStage);
-  //   (async () => {
-  //     try {
-  //       if (!currentStage)
-  //         throw Error(`currentStage is null. stageId is: ${stageId}`);
-  //       const {
-  //         Page,
-  //       }: { Page: ComponentType<{ pageNum: number; stageId: number }> } =
-  //         await import(currentStage.path);
-  //       setPageComponent(() => Page);
-  //     } catch (e) {
-  //       console.error(e);
-  //     }
-  //   })();
-  // }, [location.key]);
-  // if (!PageComponent || stageInfo === undefined) {
-  //   return <div>エラー！部員をお呼びください。</div>;
-  // }
-  // return <PageComponent pageNum={pageNum} stageId={stageId} />;
+  
   const color = stageId === 1 ? "text-red-500" : "";
   return (
     <div className="text-white">
