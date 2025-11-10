3b8d9c665d1a89377ebb8cc106b675dc4485acc2
Endo-Yuanteng <210224788+Endo-Yuanteng@users.noreply.github.com>
2025-11-10 19:08:01 +0900

    ランダムを復元

--- app/routes/game.tsx
+++ app/routes/game.tsx
@@ -54,8 +54,7 @@ export default function Game() {
   const pageNum = Number(localStorage.getItem("pageNum")); // ページ番号0~8
   const anomalyCount = Number(localStorage.getItem("anomalyCount")); //異変が連続で現れた回数
   console.log(`anomalycount = ${anomalyCount}`);
-  //const stageId = stages[biasedRandom(stages, anomalyCount)].id; // ページの種類のID
-  let stageId = 34;
+  const stageId = stages[biasedRandom(stages, anomalyCount)].id; // ページの種類のID
   console.log("stageId = " + stageId);
 
   const imgRef = useRef<HTMLImageElement | null>(null);
